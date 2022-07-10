d79a7d3e7ee7339fd2cade4495ab20df3d5b1c52
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2022-07-10 15:43:13 +0200

    first simplest contract for new event submission

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -7,12 +7,12 @@ async function main() {
 
   const lockedAmount = ethers.utils.parseEther("1");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const EventManager = await ethers.getContractFactory("EventManager");
+  const eventManager = await EventManager.deploy({ value: lockedAmount });
 
-  await lock.deployed();
+  await eventManager.deployed();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("Lock with 1 ETH deployed to:", eventManager.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ contract/test/EventManager.ts
@@ -0,0 +1,51 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import {EventManager} from "../typechain-types";
+
+describe("EventManager", async () => {
+  describe("Deployment", async () => {
+    it("Should set the right owner", async function () {
+      // given
+      const [owner] = await ethers.getSigners();
+      const contract = await ethers.getContractFactory("EventManager");
+      const eventManagerContract = await contract.deploy();
+
+      // then
+      expect(await eventManagerContract.owner()).to.equal(owner.address);
+    });
+  });
+  describe("Event creation", async () => {
+    it("Should revert if fee is too low", async () => {
+      // given
+      const contract = await ethers.getContractFactory("EventManager");
+      const eventManagerContract = await contract.deploy() as EventManager;
+      const cid = ethers.utils.toUtf8Bytes("QmZkMq9PCMPbzu54recZKjr54qm4gVJJ8TnRjqP7Q4SBsA");
+
+      // then
+      await expect(eventManagerContract.createEvent(cid, 10, 100, { value: ethers.utils.parseEther("0.01") }))
+          .revertedWith("too small fee");
+    });
+    it("Should perform new event creation and emit event", async () => {
+      // given
+      const contract = await ethers.getContractFactory("EventManager");
+      const eventManagerContract = await contract.deploy() as EventManager;
+      const [requestor] = await ethers.getSigners();
+
+      const cid = ethers.utils.hexlify(ethers.utils.toUtf8Bytes("QmZkMq9PCMPbzu54recZKjr54qm4gVJJ8TnRjqP7Q4SBsA"));
+      const ticketsTotal = 100;
+      const toDate = 123456;
+
+      // when
+      const transaction = await eventManagerContract.createEvent(cid, ticketsTotal, toDate, { value: ethers.utils.parseEther("0.1") });
+
+      // then
+      expect(transaction).to.emit(eventManagerContract, 'EventCreated')
+          .withArgs(requestor.address, 0);
+
+      const event = await eventManagerContract.events(requestor.address, 0);
+      expect(event.cid).to.be.equal(cid);
+      expect(event.ticketsTotal).to.be.equal(ticketsTotal);
+      expect(event.endDate).to.be.equal(toDate);
+    });
+  });
+});
