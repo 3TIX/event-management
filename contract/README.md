e5f09d4fefa074b557dda391606d289cdfa61256
Chmilev Fedor <17650184+ChmilevFA@users.noreply.github.com>
2022-07-18 23:10:25 +0200

    update deploy (#15)

--- contract/README.md
+++ contract/README.md
@@ -1,5 +1,5 @@
 To deploy:
 ```shell
 npx hardhat run scripts/deploy.ts --network polygonMumbai
-npx hardhat verify --network polygonMumbai <contract address>
+npx hardhat verify --network polygonMumbai --constructor-args scripts/arguments.js <contract address>
 ```
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
   const EventManager = await ethers.getContractFactory("EventManager");
-  const eventManager = await EventManager.deploy();
+  const eventManager = await EventManager.deploy(["0x326c977e6efc84e512bb9c30f76e30c160ed06fb", "0x2d7882bedcbfddce29ba99965dd3cdf7fcb10a1e"]);
 
   await eventManager.deployed();
 
