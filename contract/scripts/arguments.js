7a3bb91d6deb1c67f6246fdc4a8b8c4df84f1b79
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2022-07-19 21:41:11 +0200

    contracts deploy

--- contract/scripts/arguments.js
+++ contract/scripts/arguments.js
@@ -1,3 +1,3 @@
 module.exports = [
-    ["0x326c977e6efc84e512bb9c30f76e30c160ed06fb", "0x2d7882bedcbfddce29ba99965dd3cdf7fcb10a1e"],
+    ["0x326c977e6efc84e512bb9c30f76e30c160ed06fb", "0x2d7882bedcbfddce29ba99965dd3cdf7fcb10a1e", "0x2e3b96150C4D14C07781956cE4779E2a92CA1B23"],
 ];
