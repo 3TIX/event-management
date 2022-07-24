137d6cca9f354c4fffa232039d29cc8e1d27b91f
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2022-07-24 15:41:52 +0200

    remove WETH

--- ui/src/utils/constants.ts
+++ ui/src/utils/constants.ts
@@ -10,7 +10,6 @@ export const CURRENCIES: Record<string, string> = {
   [NATIVE_CURRENCY]: "MATIC",
   "0x2d7882bedcbfddce29ba99965dd3cdf7fcb10a1e": "TST",
   "0x326c977e6efc84e512bb9c30f76e30c160ed06fb": "LINK",
-  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": "WETH",
   "0x2e3b96150c4d14c07781956ce4779e2a92ca1b23": "HFS2022",
 }
 
