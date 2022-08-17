9ff08c14b451f058cef2fe625ebfa05c609f05c3
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-08-17 14:52:54 +0200

    Update assets (#38)

--- ui/src/pages/CreateTickets/QRTicket.tsx
+++ ui/src/pages/CreateTickets/QRTicket.tsx
@@ -1,37 +1,37 @@
 import { Box, Button, Heading, HStack, Text } from "@chakra-ui/react"
 import { ExternalLinkIcon } from "@chakra-ui/icons"
-import { ArrowRight, DiscordIcon } from "../../components/Icon"
+import { ArrowRight, GitHubIcon } from "../../components/Icon"
 
 export const QRTicket = () => {
   return (
     <Box bgImage="assets/TicketBg.png" width={347} height={547}>
       <Box bgImage="assets/QrCodeMain.png" width={227} height={227} m="60px" />
       <Box mx={8} mt={20} color="black">
-        <Heading>HackFS 2022</Heading>
+        <Heading>3TIX Hackathon</Heading>
         <Text mt={2} color="subHeader">
           Where
         </Text>
         <Button
           variant="ghost"
           rightIcon={<ExternalLinkIcon />}
-          leftIcon={<DiscordIcon />}
+          leftIcon={<GitHubIcon />}
           p={0}
           height="auto"
           _hover={{ bg: "transparent" }}
           _active={{ bg: "transparent" }}
           as="a"
           target="_blank"
-          href="https://discord.gg/ethglobal-554623348622098432"
+          href="https://github.com/3TIX/event-management"
         >
-          ETHGlobal Discord
+          3TIX Metaverse
         </Button>
         <Text mt={2} color="subHeader">
           When
         </Text>
         <HStack spacing={1}>
-          <Text fontWeight={600}>JUL 08, 2022</Text>
+          <Text fontWeight={600}>JUL 01, 2022</Text>
           <ArrowRight color="black" height="12px" />
-          <Text fontWeight={600}>JUL 29, 2022</Text>
+          <Text fontWeight={600}>JUL 31, 2022</Text>
         </HStack>
       </Box>
     </Box>
